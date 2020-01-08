6449ee3b2cc29210f64dded82224aad0af70a492
Sebastian Larrieu <slarrieu@team.neocomplexx.com>
2020-01-08 11:13:08 -0300

    Fix import

--- projects/ngx-neo-frontend-mat/schematics/layout/files/layout/administrador/administrador.module.ts
+++ projects/ngx-neo-frontend-mat/schematics/layout/files/layout/administrador/administrador.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 import { AdministradorComponent } from './administrador.component';
 import { AdministradorRoutingModule } from './administrador-routing.module';
 import { NgxNeoModalMatModule } from '@neocomplexx/ngx-neo-modal-mat';
+import { MaterialSharedModule } from 'src/app/shared/material/material.module';
 
 @NgModule({
   declarations: [AdministradorComponent],
   imports: [
     CommonModule,
     AdministradorRoutingModule,
-    NgxNeoModalMatModule
+    NgxNeoModalMatModule,
+    MaterialSharedModule
   ]
 })
 export class AdministradorModule { }
--- projects/ngx-neo-frontend-mat/schematics/layout/files/layout/user/user.module.ts
+++ projects/ngx-neo-frontend-mat/schematics/layout/files/layout/user/user.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UserComponent } from './user.component';
 import { UserRoutingModule } from './user-routing.module';
+import { MaterialSharedModule } from 'src/app/shared/material/material.module';
 
 @NgModule({
   declarations: [UserComponent],
   imports: [
     CommonModule,
-    UserRoutingModule
+    UserRoutingModule,
+    MaterialSharedModule
   ]
 })
 export class UserModule { }
